256fdf03a31ee6e12772ff31463bee267c9d6288
Cinthyavmf <77696300+Cinthyavmf@users.noreply.github.com>
2022-10-14 13:56:20 -0300

    Update Roles.js
    change model name (uppercase and singular)

--- api/src/models/Roles.js
+++ api/src/models/Roles.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo//
   sequelize.define(
-    "roles",
+    "Role",
     {
       id: {
         type: DataTypes.UUID,
