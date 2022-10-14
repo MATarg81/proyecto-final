4837e00d875c1c7ce8f300062a96d56821fd3c6b
Cinthyavmf <77696300+Cinthyavmf@users.noreply.github.com>
2022-10-14 13:55:16 -0300

    Update ProductsCategory.js
    change model name (uppercase and singular)

--- api/src/models/ProductsCategory.js
+++ api/src/models/ProductsCategory.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo//
   sequelize.define(
-    "productsCategory",
+    "ProductsCategory",
     {
       id: {
         type: DataTypes.UUID,
