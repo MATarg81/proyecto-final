113914e27a8d273a59f4a348e2ba98ad5058f82b
Azrux <lausofimarce@gmail.com>
2022-10-15 21:57:39 -0300

    Some changes

--- api/index.js
+++ api/index.js
@@ -21,7 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
+  console.log('DB on')
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
--- api/src/routes/controllers/users.js
+++ api/src/routes/controllers/users.js
@@ -1,23 +1,26 @@
 const { User, Role } = require("../../db");
 const { Op } = require("sequelize");
-const dbData = require(""); //completar!!!
+const jsonData = require("../../../users.json");
 
 async function getUsers(req, res) {
   const { name, lastname, email } = req.query;
+  const dbUsers = await User.count();
 
   try {
-    const usersData = dbData.map((u) => {
-      return {
-        email: u.email,
-        name: u.name,
-        lastname: u.lastname,
-        dateOfBirth: u.dateOfBirth,
-        phoneNumber: u.phoneNumber,
-        adress: u.adress.toString(),
-      };
-    });
-    await User.bulkCreate(usersData);
-    if (name || lastname || email) {
+    if (!dbUsers) {
+      const usersData = jsonData.results.map((u) => {
+        return {
+          email: u.email,
+          name: u.name,
+          lastname: u.lastname,
+          dateOfBirth: u.dateOfBirth,
+          phoneNumber: u.phoneNumber,
+          adress: u.adress,
+        };
+      });
+      await User.bulkCreate(usersData);
+      return res.status(200).send(usersData)
+    } else if (name || lastname || email) {
       const userName = await User.findOne({
         where: { name: { [Op.iLike]: `%${name}%` } },
       });
@@ -44,7 +47,6 @@ async function getUsers(req, res) {
   }
 }
 
-// La id tal vez deba ser username... sino por params buscariamos '/user/juanitosanchez@gmail.com' ?
 async function getUsersById(req, res) {
   const { email } = req.params;
   const findUser = await User.findOne({
@@ -57,22 +59,22 @@ async function getUsersById(req, res) {
 }
 
 async function addUser(req, res) {
-  const { name, lastname, email, dateOfBirth, rol, adress, phoneNumber } =
+  const { name, lastname, email, dateOfBirth, role, adress, phoneNumber } =
     req.body;
   const dbUser = await User.findOne({ where: { email: email }, include: Role });
 
   try {
     if (!dbUser) {
       const newUser = await User.create({
-        name,
-        lastname,
-        email,
-        dateOfBirth,
-        rol,
-        adress,
-        phoneNumber,
+        name: name,
+        lastname: lastname,
+        email: email,
+        dateOfBirth: dateOfBirth,
+        role: role,
+        adress: adress,
+        phoneNumber: phoneNumber,
       });
-      await newUser.addRole(rol);
+      await newUser.addRole(role);
       return res
         .status(200)
         .send(`User "${newUser.name + newUser.lastname}" added`);
@@ -82,12 +84,12 @@ async function addUser(req, res) {
         .send(`User "${dbUser.name + dbUser.lastname}" already exists`);
     }
   } catch (e) {
-    return res.send(e);
+    return res.status(404).send(e);
   }
 }
 
 function updateUser(req, res) {
-  const { email, name, lastname, dateOfBirth, phoneNumber, adress, rol } =
+  const { email, name, lastname, dateOfBirth, phoneNumber, adress, role } =
     req.body;
 
   const findUser = User.findOne({ where: { email: email }, include: Role });
@@ -99,7 +101,7 @@ function updateUser(req, res) {
       dateOfBirth: dateOfBirth,
       phoneNumber: phoneNumber,
       adress: adress,
-      rol: rol,
+      role: role,
     });
     return res.status(200).send(`User ${name + lastname} updated correctly`);
   } else {
