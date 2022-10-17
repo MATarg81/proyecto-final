829f6de12a0274f7811a4864ea8de1cbb63621e8
Cinthyavmf <cinthyavmf@gmail.com>
2022-10-17 19:55:05 -0300

    products: order by name, order by price, pagination

---
+++ front/src/components/Pagination.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useState} from 'react';
+
+const Pagination = function({page, setPage, totalPages}) {
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+
+    const handleNext = () => {
+        setCurrentPage(currentPage + 1);
+        setPage(page + 1);
+    }
+
+    const handlePrev = () => {
+        setCurrentPage(currentPage - 1);
+        setPage(page - 1);
+    }
+
+    let pages = [];
+    let n = 1;
+
+    while(n < totalPages + 1) {
+        pages.push(n);
+        n++;
+    }
+
+    return (
+
+    <div>
+        <div>
+            <button onClick = {handlePrev} disabled = {currentPage <= 1}>Prev.</button>
+            {pages.map((p) => <button onClick = {() => {setCurrentPage(p); setPage(p)}} disabled={currentPage === p}>{p}</button>)}
+            <button onClick = {handleNext}disabled = {currentPage >= totalPages}>Next</button>
+        </div>
+    </div>
+)}
+
+export default Pagination;
--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,92 +1,109 @@
 import React, { useState, useEffect } from "react";
+import {useHistory} from 'react-router-dom';
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/actions";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 
-function Product() {
-  const { id } = useParams();
-  const [product, setProduct] = useState([]);
-  const [loading, setloading] = useState(false);
+function Product(props) {
+  // const { id } = useParams();
+  // const [product, setProduct] = useState([]);
+  // const [loading, setloading] = useState(false);
 
-  const dispatch = useDispatch();
-  const addProduct = (product) => {
-    dispatch(addCart(product));
-  };
+  // const dispatch = useDispatch();
+  // const addProduct = (product) => {
+  //   dispatch(addCart(product));
+  // };
 
-  useEffect(() => {
-    const getProduct = async () => {
-      setloading(true);
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-      setProduct(await response.json());
-      setloading(false);
-    };
-    getProduct();
-  }, []);
+  // useEffect(() => {
+  //   const getProduct = async () => {
+  //     setloading(true);
+  //     const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  //     setProduct(await response.json());
+  //     setloading(false);
+  //   };
+  //   getProduct();
+  // }, []);
 
-  const Loading = () => {
+  // const Loading = () => {
+  //   return (
+  //     <>
+  //       <div className="col-md-6">
+  //         <Skeleton height={400} />
+  //       </div>
+  //       <div className="col-md-6" style={{ lineHeight: 2 }}>
+  //         <Skeleton height={50} width={300} />
+  //         <Skeleton height={75} />
+  //         <Skeleton height={25} width={150} />
+  //         <Skeleton height={50} />
+  //         <Skeleton height={150} />
+  //         <Skeleton height={50} width={100} />
+  //         <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+  //       </div>
+  //     </>
+  //   );
+  // };
+
+  // const ShowProduct = () => {
+  //   return (
+  //     <>
+  //       <div className="col-md-6">
+  //         <img
+  //           src={product.image}
+  //           alt={product.title}
+  //           height="400px"
+  //           width="400px"
+  //         />
+  //       </div>
+  //       <div className="col-md-6">
+  //         <h4 className="text-uppercase text-black-50">{product.category}</h4>
+  //         <h1 className="display-5">{product.title}</h1>
+  //         <p className="lead fw-bolder">
+  //           Rating {product.rating && product.rating.rate}
+  //           <i className="fa fa-star"></i>
+  //         </p>
+  //         <h3 className="display-6 fw-bold my-4">$ {product.price}</h3>
+  //         <p className="lead">{product.description}</p>
+  //         <button
+  //           className="btn btn-outline-dark px-4 py-2"
+  //           onClick={() => addProduct(product)}
+  //         >
+  //           Agregar al carrito
+  //         </button>
+  //         <Link to="/carrito" className="btn btn-dark ms-2 px-3 py-2">
+  //           Ir al carrito
+  //         </Link>
+  //       </div>
+  //     </>
+  //   );
+  // };
+
+  // return (
+  //   <div>
+  //     <div className="container py-5">
+  //       <div className="row py-4">
+  //         {loading ? <Loading /> : <ShowProduct />}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
     return (
-      <>
-        <div className="col-md-6">
-          <Skeleton height={400} />
-        </div>
-        <div className="col-md-6" style={{ lineHeight: 2 }}>
-          <Skeleton height={50} width={300} />
-          <Skeleton height={75} />
-          <Skeleton height={25} width={150} />
-          <Skeleton height={50} />
-          <Skeleton height={150} />
-          <Skeleton height={50} width={100} />
-          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
-        </div>
-      </>
-    );
-  };
-
-  const ShowProduct = () => {
-    return (
-      <>
-        <div className="col-md-6">
-          <img
-            src={product.image}
-            alt={product.title}
-            height="400px"
-            width="400px"
-          />
-        </div>
-        <div className="col-md-6">
-          <h4 className="text-uppercase text-black-50">{product.category}</h4>
-          <h1 className="display-5">{product.title}</h1>
-          <p className="lead fw-bolder">
-            Rating {product.rating && product.rating.rate}
-            <i className="fa fa-star"></i>
-          </p>
-          <h3 className="display-6 fw-bold my-4">$ {product.price}</h3>
-          <p className="lead">{product.description}</p>
-          <button
-            className="btn btn-outline-dark px-4 py-2"
-            onClick={() => addProduct(product)}
-          >
-            Agregar al carrito
-          </button>
-          <Link to="/carrito" className="btn btn-dark ms-2 px-3 py-2">
-            Ir al carrito
-          </Link>
-        </div>
-      </>
-    );
-  };
-
-  return (
-    <div>
-      <div className="container py-5">
-        <div className="row py-4">
-          {loading ? <Loading /> : <ShowProduct />}
-        </div>
-      </div>
-    </div>
-  );
+        <div>
+            <div>
+                <h4>{props.name}</h4>
+                <h4>{props.price}</h4>
+                    <div>
+                        <div>
+                            <img src ={props.img} alt={props.names} width='200px'/>
+                        </div>
+                </div>
+            </div> 
+        </div> 
+    )
+    
 }
+
 
 export default Product;
