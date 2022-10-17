338c7ef77f044b362f711ee9e1fd811d3d2b78ef
wellnahuel <nahuelcittadino@gmail.com>
2022-10-17 16:10:45 -0300

    footer(falta), clean carousel, clean about/team

--- front/src/components/Carousel.jsx
+++ front/src/components/Carousel.jsx
@@ -12,22 +12,22 @@ function Carousel() {
     <div class="carousel-item active" data-bs-interval="10000">
       <img src="/img/noticia1.jpg" class="h-50 d-inline-block" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5>Voley</h5>
+        <p></p>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="6000">
       <img src="/img/noticia2.jpg" class="h-50 d-inline-block" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <h5>Futbol</h5>
+        <p></p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="/img/noticia3.jpg" class="h-50 d-inline-block" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <h5>Basquet</h5>
+        <p></p>
       </div>
     </div>
   </div>
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -30,7 +30,7 @@ function NavBar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link active" aria-current="page" to="/home">
                   Home
                 </Link>
               </li>
