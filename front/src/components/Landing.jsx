cc9d767df264f3e12e91a6bf23f8fe6bc7e2b63e
wellnahuel <nahuelcittadino@gmail.com>
2022-10-20 10:19:08 -0300

    landing styles 90%

--- front/src/components/Landing.jsx
+++ front/src/components/Landing.jsx
@@ -8,30 +8,37 @@ function Landing() {
             <div class="container">
                 <main class="row mb-4">
                     <div class="col-md-6 text-center">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/IWnM_upHg4M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/iI-od-q_0Dw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <img src="" alt="" id="main-image" />
                         <img
-                                    src="/img/pibes club3.jpg"
-                                    alt="pibes"
-                                    height="275px"
-                                    style={{borderRadius:"10px"}}
-                                />
+                            src="/img/pibes club3.jpg"
+                            alt="pibes"
+                            height="275px"
+                            style={{ borderRadius: "10px" }}
+                        />
                     </div>
                     <div class="col-md-6 text-center my-auto">
+
+
 
                         <h1 class="font-weight-bolder">Club Athenas</h1>
                         <p>El Club Athenas es una institución que actúa, fomenta y desarrolla actividades socio-culturales y deportivas.</p>
                         <p>Desde 2022</p>
 
-                        <div class="d-flex flex-column">
 
-                            <Link to='/home'>
-                                <button class="btn btn-primary"
-                                    style={{ backgroundColor: '#352d39', borderColor: 'white' }}>Entrar</button>
-                            </Link>
+
+
+                        <Link to='/home'>
+                            <button class="btn btn-primary"
+                                style={{ backgroundColor: '#352d39', borderColor: 'white' }}>Entrar</button>
+                        </Link>
+
+                        <div class="d-flex flex-column" >
+
+
                             <div>
                                 <img
-                                    src="/img/athenas_logo.png"
+                                    src="/img/athenas_logo-removebg-preview.png"
                                     alt="Logo"
                                     height="400px"
                                 />
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -12,77 +12,77 @@ function NavBar() {
 
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-light py-1" style={{padding: "0px 0px 0px 0px"}} >
-        <div class="container">
-          <Link to="/">
-            <img
-              src="/img/athenas_logo.png"
-              alt="Logo"
-              height="80px"
-            />
-          </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+      <nav class="navbar navbar-expand-md bg-light py-1 mr-0" style={{margin:"0px"}}>
+          <div class= "container p-0" style={{background: "linear-gradient(270deg, rgba(255,255,255,1) 0%, rgba(191,173,183,1) 52%, rgba(255,173,182,1) 66%, rgba(255,255,255,1) 83%)"}}>
+      <Link to="/">
+        <img
+          src="/img/athenas_logo.png"
+          alt="Logo"
+          height="80px"
+        />
+      </Link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
 
-                <Link class="nav-link active" aria-current="page" to="/home">
-                  Home
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/tienda">
-                  Tienda
-                </Link>
-              </li>
+            <Link class="nav-link active" aria-current="page" to="/home">
+              Home
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/tienda">
+              Tienda
+            </Link>
+          </li>
 
-              <li class="nav-item">
-                <Link class="nav-link" to="/actividades">
-                  Actividades
-                </Link>
-              </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/actividades">
+              Actividades
+            </Link>
+          </li>
 
-              <li class="nav-item">
-                <Link class="nav-link" to="/crearProducto">
-                  Crear Producto
-                </Link>
-              </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/crearProducto">
+              Crear Producto
+            </Link>
+          </li>
 
-              <li class="nav-item">
-                <Link class="nav-link" to="/nosotros">
-                  Nosotros
-                </Link>
-              </li>
-            </ul>
-            <div class="buttons">
-              {/* <Link to="/ingreso" class="btn btn-outline-dark">
+          <li class="nav-item">
+            <Link class="nav-link" to="/nosotros">
+              Nosotros
+            </Link>
+          </li>
+        </ul>
+        <div class="buttons">
+          {/* <Link to="/ingreso" class="btn btn-outline-dark">
                 <i class="fa fa-sign-in me-1"></i>Ingresar
               </Link> */}
-              {/* <Link to="/registro" class="btn btn-outline-dark ms-2">
+          {/* <Link to="/registro" class="btn btn-outline-dark ms-2">
                 <i class="fa fa-user-plus me-1"></i>Registrarse
               </Link>
               {isAuthenticated ? <LogoutButton /> : <LoginButton />} */}
 
-              {/* <Profile></Profile> */}
-              <Link to="/carrito" class="btn btn-outline-dark ms-2">
-                <i class="fa fa-shopping-cart me-1"></i>Carrito (
-                {state.length})
-              </Link>
-            </div>
-          </div>
+          {/* <Profile></Profile> */}
+          <Link to="/carrito" class="btn btn-outline-dark ms-2">
+            <i class="fa fa-shopping-cart me-1"></i>Carrito (
+            {state.length})
+          </Link>
         </div>
-      </nav>
+      </div>
     </div>
+      </nav >
+    </div >
   );
 }
 
