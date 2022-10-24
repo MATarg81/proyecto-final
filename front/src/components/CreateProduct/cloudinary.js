6fd88d24b86b86d7e4f3c86ca67e241c2150a7e9
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2022-10-24 18:16:14 -0300

    formulario producto

--- front/src/components/CreateProduct/cloudinary.js
+++ front/src/components/CreateProduct/cloudinary.js
@@ -1,14 +1,26 @@
-import axios from "axios";
 
-const upImage = async (files) => {
-  let file = files[0];
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append("upload_preset", "dzux3ynpe");
+export async function upImage(obj) {
+  let file = obj
+  const formData = new FormData()
+  formData.append('file', file)
+  formData.append('upload_preset', 'preset')
+  let myHeaders = new Headers();
+  let options = {
+    method: 'POST',
+    body: formData,
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+  };
+  try {
+    const res = await fetch('https://api.cloudinary.com/v1_1/dzux3ynpe/image/upload', options)
+    const data = await res.json()
+    return data.secure_url
+  } catch (error) {
+    return new Error("Error al cargar la imagen")
+  }
 
-  axios.post(
-    "https://api.cloudinary.com/v1_1/dzux3ynpe/image/upload",
-    formData.then((response) => {console.log(response.json(), " soy response")})
-  );
-};
+}
+
+  
 export default upImage;
