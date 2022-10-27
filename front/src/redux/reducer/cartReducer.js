e139a420cdb586f6f6458be950db8f73be836681
Azrux <lausofimarce@gmail.com>
2022-10-27 00:18:48 -0300

    Ternario cart

--- front/src/redux/reducer/cartReducer.js
+++ front/src/redux/reducer/cartReducer.js
@@ -1,8 +1,11 @@
 import { ADD_ITEM, DELETE_ITEM, DELETE_ALL, POST_CART } from "../actionsTypes/actionsTypesCart";
 
 const inicialState = {
-  items:JSON.parse(localStorage.cart),
+  items:localStorage.cart 
+  ? JSON.parse(localStorage.cart)
+  : [],
   price: 0,
+  // items:JSON.parse(localStorage.cart),
 };
 
 const cartReducer = (state = inicialState, action) => {
