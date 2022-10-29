ee4e851c4e092ad40e42c2e9ff27bccc189671c2
wellnahuel <nahuelcittadino@gmail.com>
2022-10-29 17:53:36 -0300

    Me trabe y no le encuentro la vuelta, dejo link en trello con los errores, y una explicacion general

--- front/src/components/Login/EditButton.jsx
+++ front/src/components/Login/EditButton.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+//import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom'
 
 const EditButton = () => {
 
-    const { edit } = useAuth0();
+    // const { edit } = useAuth0();
 
-    return (
-        <Link to="/edituser">
-            <button className="btn btn-outline-dark ms-2" onClick={() => edit()}>
-                <i class=""></i>Editar Perfil
-            </button>
-        </Link>
-    )
-}
+    function handleOnClick(e) {
+        
+    }
 
-export default EditButton;
+        return (
+            <Link to="/edituser">
+                <button className="btn btn-outline-dark ms-2" /* onClick={() => handleOnClick(e)} */>
+                    <i class=""></i>Editar Perfil
+                </button>
+            </Link>
+        )
+    }
+
+    export default EditButton;
 
