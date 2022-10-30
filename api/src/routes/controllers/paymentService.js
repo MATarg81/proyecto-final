9a8050c55111c137c8a449629ab668599e6248c9
MATarg <matiastula@gmail.com>
2022-10-30 13:26:07 -0300

    Routes MP back

---
+++ api/src/routes/controllers/paymentService.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+
+class PaymentService {
+  async createPayment() {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+      payer_email: "test_user_10178403@testuser.com",
+      items: [
+        //Acá hay que traer los productos reales
+        //También se pueden modificar las formas de pago
+        {
+          title: "Producto de prueba",
+          description: "Descripción del producto de prueba",
+          picture_url: "http://www.myapp.com/myimage.jpg",
+          category_id: "category123",
+          quantity: 1,
+          unit_price: 10,
+        },
+      ],
+      back_urls: {
+        failure: "https://proyecto-final-git-develop-matarg81.vercel.app",
+        pending: "https://proyecto-final-git-develop-matarg81.vercel.app",
+        success: "https://proyecto-final-git-develop-matarg81.vercel.app",
+      },
+    };
+
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+    });
+
+    return payment.data;
+  }
+
+  async createSubscription() {
+    const url = "https://api.mercadopago.com/preapproval";
+
+    const body = {
+      reason: "Cuota mensual",
+      auto_recurring: {
+        frequency: 1,
+        frequency_type: "months",
+        transaction_amount: 10,
+        currency_id: "ARS",
+      },
+      back_url: "https://proyecto-final-git-develop-matarg81.vercel.app",
+      payer_email: "test_user_10178403@testuser.com",
+    };
+
+    const subscription = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+    });
+
+    return subscription.data;
+  }
+}
+
+module.exports = PaymentService;
