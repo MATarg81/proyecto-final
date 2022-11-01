d273f85de1f9a4b0aa811970e6ccefb043849549
Cinthyavmf <cinthyavmf@gmail.com>
2022-11-01 16:54:05 -0300

    route reviews products

--- api/src/routes/controllers/reviews.js
+++ api/src/routes/controllers/reviews.js
@@ -1,8 +1,8 @@
-const { Review , User, Activity } = require("../../db");
+const { Review , User, Product, Activity } = require("../../db");
 const jsonData = require("../../../reviews.json");
 
 
-const allReviews = async() => {
+const allActivitiesReviews = async() => {
   const dbData = await Review.count();
 
     try {
@@ -10,7 +10,7 @@ const allReviews = async() => {
             // const results = await Review.bulkCreate(jsonData.results);
             // return results;
           const results = jsonData.results.map(async r => {
-            const findAct = await Activity.findOne({where: {name: r.activity}})
+            const findAct = await Activity.findOne({where: {name: r.activity}});
             const newRev = await Review.create({
               score: r.score,
               content: r.content
@@ -28,9 +28,9 @@ const allReviews = async() => {
     };
 }
 
-const reviewsId = async(id) => {
+const reviewsActivitiesId = async(id) => {
   try {
-      const totalReviews = await allReviews();
+      const totalReviews = await allActivitiesReviews();
       const reviewsId = totalReviews.find((r) => r.activityId.toString() === id);
           
           return reviewsId;
@@ -41,10 +41,10 @@ const reviewsId = async(id) => {
 };
 
 
-async function getReviews(req, res) {
+async function getActivitiesReviews(req, res) {
 
 try{
-    const totalReviews = await allReviews();
+    const totalReviews = await allActivitiesReviews();
   
     if(totalReviews){
       return res.status(200).send(totalReviews);
@@ -57,12 +57,12 @@ try{
 };
 
 
-async function getReviewsId (req, res) {
+async function getActivitiesReviewsId (req, res) {
   try {
       const id = req.params.id;
 
       
-      const result = await reviewsId(id);
+      const result = await reviewsActivitiesId(id);
       if (result) {
           return res.status(200).send(result);
       } else {
@@ -74,7 +74,7 @@ async function getReviewsId (req, res) {
   }
 };
 
-async function addReviews(req, res) {
+async function addActivitiesReviews(req, res) {
   const { 
     score, 
     content, 
@@ -103,9 +103,115 @@ async function addReviews(req, res) {
   }
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+const allProductsReviews = async() => {
+  const dbData = await Review.count();
+
+    try {
+        if (!dbData) {   
+            // const results = await Review.bulkCreate(jsonData.results);
+            // return results;
+          const results = jsonData.results.map(async r => {
+            const findProduct = await Product.findOne({where: {name: r.product}})
+            const newRev = await Review.create({
+              score: r.score,
+              content: r.content
+            });
+            await newRev.setProduct(findProduct?.id);
+          });
+          return results;
+        } else {
+          
+            const dbReviews = await Product.findAll({include:[{model: Product, attributes: ['name'], include:[{model: User, attributes: ['name']}]}]})
+            return dbReviews;
+        }        
+    } catch (error) {
+        console.log('Problemas en la función dbReviews()' + error);
+    };
+}
+
+const reviewsProductsId = async(id) => {
+  try {
+      const totalReviews = await allProductsReviews();
+      const reviewsId = totalReviews.find((r) => r.productId.toString() === id);
+          
+          return reviewsId;
+  }
+  catch(err) {
+      console.log('Problemas en /:id' + err);
+  }
+};
+
+
+async function getProductsReviews(req, res) {
+
+try{
+    const totalReviews = await allProductsReviews();
+  
+    if(totalReviews){
+      return res.status(200).send(totalReviews);
+    } else {
+      res.status(404).send('Producto no encontrada')
+  }
+  } catch (e) {
+    return res.status(404).send('Problemas en el controlador de la ruta GET/reviews' + e);
+  }
+};
+
+
+async function getProductsReviewsId (req, res) {
+  try {
+      const id = req.params.id;
+
+      
+      const result = await reviewsProductsId(id);
+      if (result) {
+          return res.status(200).send(result);
+      } else {
+          res.status(404).send('Id no existente')
+      };
+  }
+  catch(err) {
+      res.status(404).send('Problemas en el controlador de la ruta GET/reviews/:id');
+  }
+};
+
+async function addProductsReviews(req, res) {
+  const { 
+    score, 
+    content, 
+    product
+  } = req.body;
+  const findProduct = Product.findOne({ where: { name: product}});
+
+  try {
+      const newReview = await Review.create({
+        score: score,
+        content: content,
+
+      });
+      
+      newReview.setProduct(findProduct.id);
+
+      console.log(newReview)
+
+      return res
+      .status(200)
+      .json(newReview); 
+  } catch (e) {
+    return res
+    .status(404)
+    .send(console.log(e));
+  }
+}
+
 
 module.exports = {
-  getReviews,
-  addReviews,
-  getReviewsId
+  getActivitiesReviews,
+  addActivitiesReviews,
+  getActivitiesReviewsId,
+  getProductsReviews,
+  addProductsReviews,
+  getProductsReviewsId
 };
