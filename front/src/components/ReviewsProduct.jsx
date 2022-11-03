16fffbe95854b3400bd9950d6c981cdb8847f6cb
Cinthyavmf <cinthyavmf@gmail.com>
2022-11-03 14:32:05 -0300

    reviews in product component

---
+++ front/src/components/ReviewsProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+const Reviews = () => {
+    
+
+}
+
+export default Reviews;
