f5153d99968fecc3d966af8a77684a8010d0dc33
wellnahuel <nahuelcittadino@gmail.com>
2022-11-05 17:37:58 -0300

    probando estilos definitivos 4 toastify

--- front/src/components/Shop.jsx
+++ front/src/components/Shop.jsx
@@ -101,7 +101,17 @@ function Shop() {
     //ADDtoFavs(p)
     dispatch(addFav(id));
     //localStorage.setItem('favs', JSON.stringify(favState))
-    alert("Producto agregado a favoritos");
+    alert("Producto agregado a favoritos")
+    
+    /* toast('🦄 Producto añadido a favoritos', {
+      position: "top-right",
+      autoClose: 800,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    }); */
   }
 
   // useEffect(() => { //si cambia el estado local FavState , entonces setIteame el LS
@@ -196,14 +206,14 @@ function Shop() {
 
         <div class="row align-items-center py-3 px-xl-5">
           <div class="col-lg-3 d-none d-lg-block" >
-            <h1 class="m-0 display-5 font-weight-semi-bold"><span class="font-weight-bold border px-1 mr-1" style={{color: "indigo"}}>T</span>ienda</h1>
+            <h1 class="m-0 display-5 font-weight-semi-bold"><span class="font-weight-bold border px-1 mr-1" style={{ color: "indigo" }}>T</span>ienda</h1>
           </div>
           <div class="col-lg-6 col-6 text-left ">
             <SearchBar setPage={setPage} />
             <select
               onChange={order}
               className="btn border dropdown-toggle rounded-pill text-white"
-              style={{backgroundColor: "indigo"}}
+              style={{ backgroundColor: "indigo" }}
             >
               <option defaultValue="ordenar">Ordenar por:</option>
               <option value="A/Z">A/Z</option>
@@ -213,7 +223,7 @@ function Shop() {
             </select>
             <select
               onChange={filterCategories}
-              className="btn border dropdown-toggle rounded-pill text-white"  style={{backgroundColor: "indigo"}}>
+              className="btn border dropdown-toggle rounded-pill text-white" style={{ backgroundColor: "indigo" }}>
               <option defaultValue="Categories">Filtrar categorías</option>
               {category?.map((c) => (
                 <option name={c.name} key={c.id} value={c.name}>
@@ -224,25 +234,21 @@ function Shop() {
             <button onClick={cleanFilters} className="btn btn-outline-danger text-dark ms-2 border rounded-pill" >
               Borrar filtros
             </button>
-            
+
           </div>
           <div class="col-lg-3 col-6 text-right" data-toggle="tooltip" data-placement="bottom" title="Favoritos">
             <Link className="" to="/favorites">
 
-              <svg xmlns="http://www.w3.org/2000/svg" style={{color: "indigo"}} width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "indigo" }} width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
               </svg>
               <span class="badge text-dark ">({fav_state.favs?.length})</span>
             </Link>
-
             <Link to="/carrito" data-toggle="tooltip" data-placement="bottom" title="Carrito">
-              <svg xmlns="http://www.w3.org/2000/svg" style={{color: "indigo"}} width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "indigo" }} width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
               </svg>
             </Link>
-
-
-
             <span class="badge text-dark">({cart_state.items?.length})</span>
           </div>
         </div>
@@ -272,24 +278,24 @@ function Shop() {
               </div>
               <div class="card-footer d-flex justify-content-between border text-primary" >
                 <a onClick={() => navigate(`/tienda/${p.id}`)} class="btn btn-sm  p-0" >
-                  <i>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16" >
+                  <i data-toggle="tooltip" data-placement="bottom" title="Ver detalle" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style={{ color: "indigo" }} fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16" >
                       <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                       <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                     </svg>
                   </i>
                 </a>
                 <a onClick={() => addProduct(p)} class="btn btn-sm  p-0">
-                  <i >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                  <i data-toggle="tooltip" data-placement="bottom" title="Agregar al carrito">
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "indigo" }} data-toggle="tooltip" data-placement="bottom" title="Agregar al carrito" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                       <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                     </svg>
                   </i>
                 </a>
-                <a onClick={() => handleAddtoFav(p.id)} class="btn btn-sm  p-0">
-                  <i>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-through-heart" viewBox="0 0 16 16">
-                      <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l.53-.53c-.771-.802-1.328-1.58-1.704-2.32-.798-1.575-.775-2.996-.213-4.092C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182a21.86 21.86 0 0 1-2.685-2.062l-.539.54V14a.5.5 0 0 1-.146.354l-1.5 1.5Zm2.893-4.894A20.419 20.419 0 0 0 8 12.71c2.456-1.666 3.827-3.207 4.489-4.512.679-1.34.607-2.42.215-3.185-.817-1.595-3.087-2.054-4.346-.761L8 4.62l-.358-.368c-1.259-1.293-3.53-.834-4.346.761-.392.766-.464 1.845.215 3.185.323.636.815 1.33 1.519 2.065l1.866-1.867a.5.5 0 1 1 .708.708L5.747 10.96Z" />
+                <a onClick={() => handleAddtoFav(p.id)} class="btn btn-sm  p-0 ">
+                  <i data-toggle="tooltip" data-placement="bottom" title="Agregar a favoritos">
+                    <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "indigo" }} data-toggle="tooltip" data-placement="bottom" title="Agregar a favoritos" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                     </svg>
                   </i>
                 </a>
