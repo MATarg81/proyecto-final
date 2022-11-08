d1c71edd2e83f684bfb20008aa494aac8fa0aad7
MATarg <matiastula@gmail.com>
2022-11-07 21:35:53 -0300

    Probando

--- front/src/components/MpSuccess.jsx
+++ front/src/components/MpSuccess.jsx
@@ -1,23 +1,25 @@
-// import React, { useEffect } from "react";
-// import { useLocalStorage } from "../localStorage/useLocalStorage";
-// import { useSelector, useDispatch } from "react-redux";
-// import { delAll } from "../redux/actionsCreator/cartActions";
+import React, { useEffect } from "react";
+import { useLocalStorage } from "../localStorage/useLocalStorage";
+import { useSelector, useDispatch } from "react-redux";
+import { delAll, postCart } from "../redux/actionsCreator/cartActions";
 
 function MpSuccess() {
-//   const state = useSelector((state) => state.cartReducer.items);
-//   const [, setCart] = useLocalStorage("cart", state);
-//   const dispatch = useDispatch();
+  const state = useSelector((state) => state.cartReducer.items);
+  const [, setCart] = useLocalStorage("cart", state);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     if (state.length !== 0) {
-//       setCart([]);
-//       dispatch(delAll());
-//     }
-//   }, []);
+    useEffect(() => {
+        dispatch(postCart(state))
+    }, [])
 
-  return (
-  <div>El pago fue exitoso!</div>
-  )
+  useEffect(() => {
+    if (state.length !== 0) {
+      setCart([]);
+      dispatch(delAll());
+    }
+  }, []);
+
+  return <div>El pago fue exitoso!</div>;
 }
 
 export default MpSuccess;
