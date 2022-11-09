b17618a5716fb3e4522cbe3b5749b9bc977ec4b4
wellnahuel <nahuelcittadino@gmail.com>
2022-11-09 00:14:03 -0300

    boton de cart sin login2

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -132,44 +132,8 @@ const Cart = () => {
               </button>
             </div>
           </div>
-          {/*           <div className="p-2">Flex item 3</div>
-           */}
-        </div>
-
-        {/*  <div className="px-4 my-5 bg-light rounded-3 py-5">
-          <div className="container py-4">
-            <div className="row justify-content-center">
-              <div className="col-md-4">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  height="200px"
-                  width="180px"
-                />
-              </div>
-              <div className="col-md-4">
-                <h3>{product.name}</h3>
-                <p className="lead fw-bold">
-                  {product.qty} x $ {product.price} = $
-                  {product.qty * product.price}
-                </p>
-                <hr />
-                <button
-                  className="btn btn-outline-dark me-2"
-                  onClick={() => handleDel(product)}
-                >
-                  <i className="fa fa-minus"></i>
-                </button>
-                <button
-                  className="btn btn-outline-dark"
-                  onClick={() => handleAdd(product)}
-                >
-                  <i className="fa fa-plus"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div> */}
+         
+        </div>        
       </>
     );
   };
