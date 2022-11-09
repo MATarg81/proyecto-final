a2fc46d017d85f9dbbe856fa5b3ef980b991814f
wellnahuel <nahuelcittadino@gmail.com>
2022-11-09 20:09:21 -0300

    fix controller users y category

--- api/src/routes/controllers/category.js
+++ api/src/routes/controllers/category.js
@@ -1,20 +1,51 @@
 const { Category} = require("../../db");
 const jsonData = require("../../../category.json");
-const { Op } = require("sequelize");
 
 async function getCategory(req, res) {
+  const dbData = await Category.count();
 
   try {
-    const allCategories = await Category.findAll();
-    res.status(200).send(allCategories);
+    if (!dbData) {
+      // const categoryData = jsonData.results.map((r) => {
+      //   return {
+      //     name: r.name,
+      //   };
+      // });
+      const charged = await Category.bulkCreate(jsonData.results);
+      res.status(200).json(charged);
+    } else {
+      const allCategories = await Category.findAll();
+      res.status(200).send(allCategories);
+    }
   } catch (e) {
     res.status(404).send(e);
   }
 }
 
+async function getAllCategories(req, res){
+  const dbfilled = await Category.findAll()
+  try {
+    if(dbfilled.length === 0){
+       await getCategory()
+       let lleno = Category.findAll()
+
+      if(dbfilled.length > 0) return res.status(200).json(lleno)
+    }else{
+      console.log(dbfilled, " filled")
+      return res.status(200).json(dbfilled)
+
+    }
+
+  } catch (error) {
+    console.log(error, "soy error")
+    return res.status(400).send('get All Categories db error').json(error)
+  }
+
+}
+
 async function addCategory(req, res) {
   const {name} = req.body
-  const findCategory = await Category.findOne({where: {name: { [Op.iLike]: `%${name}%` }}})
+  const findCategory = await Category.findOne({where: {name: name}})
   try{
     if(!findCategory) {
       const newcategory = await Category.create({
@@ -29,4 +60,4 @@ async function addCategory(req, res) {
   }
 }
 
-module.exports = {getCategory, addCategory};
+module.exports = {getCategory, getAllCategories, addCategory};
