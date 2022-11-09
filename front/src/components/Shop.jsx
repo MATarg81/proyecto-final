91703b3de27886a0505e1048cd5377ee42471063
Cinthyavmf <cinthyavmf@gmail.com>
2022-11-08 21:17:16 -0300

    Filters fixed

--- front/src/components/Shop.jsx
+++ front/src/components/Shop.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Pagination from "./Pagination";
-//import { addCart } from "../redux/actions/index";
 import { addFav, deleteFav } from "../redux/actionsCreator/favsActions";
 import { addCart } from "../redux/actionsCreator/cartActions";
 import {
@@ -59,13 +58,7 @@ function Shop() {
 
   // --------------- Pagination --------------
   const productsPerPage = 12;
-
-  const totalPages =
-    byCategories.length > 0
-      ? Math.ceil(byCategories?.length / productsPerPage)
-      : price.length > 0
-      ? Math.ceil(price?.length / productsPerPage)
-      : Math.ceil(products?.length / productsPerPage);
+  const totalPages = Math.ceil(products.length/productsPerPage);
 
   const [, setOrder] = useState();
   const [input, setInput] = useState({
@@ -76,12 +69,7 @@ function Shop() {
   const [page, setPage] = useState(1);
   const first = (page - 1) * productsPerPage;
   const last = page * productsPerPage;
-  let productsPage =
-    byCategories.length > 0
-      ? byCategories?.slice(first, last)
-      : price.length > 0
-      ? price?.slice(first, last)
-      : products?.slice(first, last);
+  const productsPage = products.slice(first, last);
 
   // --------------- Data call -------------
 
@@ -133,9 +121,7 @@ function Shop() {
     }); */
 
   const stateOrLs = findUser ? fav_state : fav_LS_state;
-  // useEffect(() => { //si cambia el estado local FavState , entonces setIteame el LS
-  //   localStorage.setItem('favs', JSON.stringify(favState))
-  // }, [favState])
+
   // --------------- Cart function ----------------
   const addProduct = (product) => {
     dispatch(addCart(product));
@@ -167,7 +153,7 @@ function Shop() {
 
   // ----------------- filter functions ------------------
   const filterCategories = (e) => {
-    e.preventDefault();
+    console.log(e.target.value)
     setPage(1);
     dispatch(filterByCategories(e.target.value));
   };
@@ -178,22 +164,6 @@ function Shop() {
       [e.target.name]: e.target.value,
     });
   }
-
-  /* const handleSumit = (e) => {
-    e.preventDefault();
-    setPage(1);
-    if (byCategories.length > 0) {
-      const filteredCategories = byCategories?.filter((c) => {
-        return parseInt(c.price) > input.min && parseInt(c.price) < input.max;
-      });
-      dispatch(filterByPrice(filteredCategories));
-    } else {
-      const filteredAll = products?.filter((p) => {
-        return parseInt(p.price) > input.min && parseInt(p.price) < input.max;
-      });
-      dispatch(filterByPrice(filteredAll));
-    }
-  }; */
 
   //--------------- clean sort and filters function --------------------
   const cleanFilters = (e) => {
