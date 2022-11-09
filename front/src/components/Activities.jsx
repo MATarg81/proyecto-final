a0b84292ca8fcdc421414c0da4684ae11fb5b54e
wellnahuel <nahuelcittadino@gmail.com>
2022-11-08 21:33:40 -0300

    falta mover los botones del costado, no estan contenidos en un div y por el ternario se complica agregarlos dentro de un div , queda pendiente

--- front/src/components/Activities.jsx
+++ front/src/components/Activities.jsx
@@ -156,7 +156,7 @@ export default function Activities() {
                       </div>
 
                       {!findUser ? (
-                        <>
+                        <div class="d-flex flex-column">
                           <div class="d-flex flex-column px-4">
                             <button
                               type="button"
@@ -189,25 +189,25 @@ export default function Activities() {
                               </div>
                             </div>
                           </div>
-                        </>
+                        </div>
                       ) : (
 
                         <button
                           type="button"
-                          className="btn btn-secondary"
+                          className="btn btn-outline-light mb-4 rounded-pill d-flex flex-column"
                           onClick={() => handleAddtoAct(a.id)}
                         >
                           Inscribirse
                         </button>
                       )}
 
-                      <div className="d-flex flex-column px-4">
+                      <div className="d-flex flex-column">
                         {!findUser ? (
                         <>
                           
                             <button
                               type="button"
-                              className=""
+                              className="btn btn-outline-light mb-4 rounded-pill "
                               data-bs-toggle="modal"
                               data-bs-target="#inscriptionActivities"
                             >
@@ -242,7 +242,7 @@ export default function Activities() {
                         {" "}
                         <button
                           type="button"
-                          className="btn btn-outline-dark ms-2"
+                          className="btn btn-outline-light mb-4 rounded-pill"
                         >
                           Deja tu reseña
                         </button>{" "}
