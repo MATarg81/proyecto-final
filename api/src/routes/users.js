6eb81eac0339bbb4b68770fede6c6cab1af6fd4b
MATarg <matiastula@gmail.com>
2022-11-09 17:10:18 -0300

    Volviendo back

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,21 +1,21 @@
 const { Router } = require("express");
 const {
   getUsers,
+  getUsersById,
   deleteUser,
   addUser,
   updateUser
 } = require("./controllers/users");
+const { verifyAdmin } = require('./controllers/middlewaresRoles')
 
 //Trae todos los users desde la DB
 //Trae los users por nombre, apellido, email,
 //Debe poder actualizar los datos
 //Debe poder eliminar users
-
 const router = Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUsersById); 
 router.post("/", addUser);
 router.delete("/:id", deleteUser);
 router.patch("/", updateUser)
-
-module.exports = router;
