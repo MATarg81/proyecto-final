ca43831da08add3a201c111a3edd00e762d9050f
MATarg <matiastula@gmail.com>
2022-11-10 13:41:54 -0300

    Users - Profile

--- api/src/routes/controllers/users.js
+++ api/src/routes/controllers/users.js
@@ -9,7 +9,7 @@ async function getUsers(req, res) {
 
   try {
     if (!dbData) {
-      const allUsers = jsonData.results.map(async u => {
+      const allUsers = jsonData.results.map(async (u) => {
         const newUser = await User.create({
           name: u.name,
           lastname: u.lastname,
@@ -18,15 +18,13 @@ async function getUsers(req, res) {
           phoneNumber: u.phoneNumber,
           address: u.address,
           postalCode: u.postalCode.toString(),
-          image: u.image
+          image: u.image,
         });
 
         await newUser.setRole(2);
-
       });
-      return res.status(200).send(allUsers)
-    }
-    else if (name || lastname || email) {
+      return res.status(200).send(allUsers);
+    } else if (name || lastname || email) {
       const userName = await User.findOne({
         where: { name: { [Op.iLike]: `%${name}%` } },
       });
@@ -48,14 +46,11 @@ async function getUsers(req, res) {
       } else {
         return res.status(404).send("User can't be found");
       }
-    }
-    else {
+    } else {
       //User.findAll({ include: Role }).then((r) => res.status(200).send(r));
-      User.findAll({
-        include: Role, include: [{
-          model: Activity
-        }]
-      }).then((r) => res.status(200).send(r));
+      User.findAll({ include: [{ model: Role }, { model: Activity }] }).then(
+        (r) => res.status(200).send(r)
+      );
     }
   } catch (error) {
     return res.status(404).send(error);
@@ -68,11 +63,11 @@ async function getUsersById(req, res) {
   const findUser = await User.findOne({
     where: { id: id },
     include: {
-      model: Activity
-    }
+      model: Activity,
+    },
   });
   if (findUser) {
-    await findUser.save()
+    await findUser.save();
     res.status(200).send(findUser);
   } else {
     res.status(404).send("User can't be found");
@@ -80,84 +75,82 @@ async function getUsersById(req, res) {
 }
 
 async function addUser(req, res) {
-  const { name, lastname, email, dateOfBirth, address, phoneNumber, postalCode, image } = req.body;
+  const {
+    name,
+    lastname,
+    email,
+    dateOfBirth,
+    address,
+    phoneNumber,
+    postalCode,
+    image,
+  } = req.body;
 
-    const dbUser = await User.findOne({ where: { email: email }, include: Role });
-    try {
-      if (!dbUser) {
-        const newUser = await User.create({
-          name: name,
-          lastname: lastname,
-          email: email,
-          dateOfBirth: dateOfBirth.toString(),
-          phoneNumber: parseInt(phoneNumber),
-          address: address,
-          postalCode: postalCode.toString(),
-          image: image
+  const dbUser = await User.findOne({ where: { email: email }, include: Role });
+  try {
+    if (!dbUser) {
+      const newUser = await User.create({
+        name: name,
+        lastname: lastname,
+        email: email,
+        dateOfBirth: dateOfBirth.toString(),
+        phoneNumber: parseInt(phoneNumber),
+        address: address,
+        postalCode: postalCode.toString(),
+        image: image,
+      });
 
-        });
-
-        const addRole = await newUser.setRole(2);
-        return res.status(200).send(newUser);
-      } else {
-        res.status(404).send(`User "${name + " " + lastname}" already exists`);
-
-
-
-
-
-
-
-
-
-      }
-    } catch (e) {
-      return res.status(404).send(console.log(e));
-    }
-  }
-    async function deleteUser(req, res) {
-    const { id } = req.params;
-    const findUser = await User.findOne({ where: { id: id } });
-    if (findUser) {
-      await findUser.destroy();
-      return res.status(200).send(`User was deleted`);
+      const addRole = await newUser.setRole(2);
+      return res.status(200).send(newUser);
     } else {
-      return res.status(404).send(`Cannot find the user email ${id}`);
+      res.status(404).send(`User "${name + " " + lastname}" already exists`);
     }
+  } catch (e) {
+    return res.status(404).send(console.log(e));
   }
+}
+async function deleteUser(req, res) {
+  const { id } = req.params;
+  const findUser = await User.findOne({ where: { id: id } });
+  if (findUser) {
+    await findUser.destroy();
+    return res.status(200).send(`User was deleted`);
+  } else {
+    return res.status(404).send(`Cannot find the user email ${id}`);
+  }
+}
 
-
-  async function updateUser(req, res) {
-    const body = req.body;
-    try {
-      await User.update(
-        {
-          name: body.name,
-          lastname: body.lastname,
-          dateOfBirth: body.dateOfBirth,
-          phoneNumber: body.phoneNumber,
-          email: body.email,
-          address: body.address,
-          postalCode: body.postalCode,
-          roleId: body.roleId,
-          image: body.image
+async function updateUser(req, res) {
+  const body = req.body;
+  try {
+    await User.update(
+      {
+        name: body.name,
+        lastname: body.lastname,
+        dateOfBirth: body.dateOfBirth,
+        phoneNumber: body.phoneNumber,
+        email: body.email,
+        address: body.address,
+        postalCode: body.postalCode,
+        roleId: body.roleId,
+        image: body.image,
+      },
+      {
+        where: {
+          id: Number(body.id),
         },
-        {
-          where: {
-            id: Number(body.id),
-          },
-        }
-      );
-      res.status(200).send("Usuario actualizado con éxito");
-    } catch (error) {
-      res.status(400).send(error);
-    }
+      }
+    );
+    res.status(200).send("Usuario actualizado con éxito");
+  } catch (error) {
+    res.status(400).send(error);
   }
+}
 
-  module.exports = {
-    getUsers,
-    getUsersById,
-    deleteUser,
-    addUser,
-    updateUser
-  };
+module.exports = {
+  getUsers,
+  getUsersById,
+  deleteUser,
+  addUser,
+  updateUser,
+};
